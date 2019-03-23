29506036a7f05c966b42904994a54d7757bf8b29
Roman <misnikovroman@gmail.com>
2019-03-23 13:08:58 +0300

    (MQTT) Добавил тестовый viewcontroller с кнопкой и лэйблом

---
+++ HealthApp/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  HealthApp
+//
+//  Created by MisnikovRoman on 23/03/2019.
+//  Copyright © 2019 MisnikovRoman. All rights reserved.
+//
+
+import Foundation
+
+enum VC {
+	static let test = "TestVC"
+}
--- HealthApp/TestVC.swift
+++ HealthApp/TestVC.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TestVC: UIViewController {
+
+	@IBOutlet weak var debugMsgLbl: UILabel!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
 
-
+	@IBAction func test(_ sender: Any) {
+		
+	}
 }
 
